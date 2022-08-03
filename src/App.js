cce8996bdd41c157382e9ae491272fbc11713e75
parag <paragpatil187@gmail.com>
2022-08-03 12:57:24 +0530

    basic start for trail

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [name, setName] = useState("");
+  const [location, setLocation] = useState("");
+  const [stream, setStream] = useState("");
+  const [school, setSchool] = useState("");
+
+  const handleInputName = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleInputLocation = (e) => {
+    setLocation(e.target.value);
+  };
+
+  const handleInputStream = (e) => {
+    setStream(e.target.value);
+  };
+
+  const handleInputSchool = (e) => {
+    setSchool(e.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Bio - Generator</h1>
+      <div className="container">
+        <div className="option">
+          <h2>Form</h2>
+          <h3>Name</h3>
+          <input
+            type="text"
+            placeholder="Enter name"
+            onChange={handleInputName}
+          />
+          <br />
+          <h3>Location</h3>
+          <input
+            type="text"
+            placeholder="Enter location"
+            onChange={handleInputLocation}
+          />
+          <br />
+          <div className="education">
+            <br />
+            <h3>School</h3>
+            <input
+              type="text"
+              placeholder="Enter School/Institute Name"
+              onChange={handleInputSchool}
+            />
+            <h3>Specialization</h3>
+            <input
+              type="text"
+              placeholder="Enter Specialization"
+              onChange={handleInputStream}
+            />
+          </div>
+        </div>
+        <div className="result">
+          <h2>Results</h2>
+          <h3>
+            {name} is from the {location}. He is studying {stream} at {school}.
+          </h3>
+        </div>
+      </div>
     </div>
   );
 }
